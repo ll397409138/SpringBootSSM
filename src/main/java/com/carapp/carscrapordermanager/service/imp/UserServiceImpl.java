49c9d9eab4057085000352fcbed66df7f76094ca
liuziliang <liuziliang@ithro.com.cn>
2019-04-30 18:04:37 +0800

    初始化项目

--- src/main/java/com/carapp/carscrapordermanager/service/imp/UserServiceImpl.java
+++ src/main/java/com/carapp/carscrapordermanager/service/imp/UserServiceImpl.java
@@ -1,8 +1,11 @@
+
+
 package com.carapp.carscrapordermanager.service.imp;
 
 import com.carapp.carscrapordermanager.mapper.UserMapper;
 import com.carapp.carscrapordermanager.pojo.User;
 import com.carapp.carscrapordermanager.service.UserService;
+import com.carapp.carscrapordermanager.vo.UserVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,8 +14,22 @@ import javax.annotation.Resource;
 @Service
 public class UserServiceImpl extends BaseServiceImpl<User> implements UserService  {
 
+
+
     @Resource
     private UserMapper userMapper;
+
+    public int insert(UserVO user){
+         Integer roleId = user.getRoleId();
+
+         //判断roleId对应的数据是否存在
+         return 1;
+    }
+
+    public int update(UserVO user){
+
+        return 1;
+    }
 
 
 }
