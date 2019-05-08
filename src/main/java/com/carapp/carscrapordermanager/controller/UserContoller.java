4d5afa52371f439bee5b481581595cc6db62d9aa
liuziliang <liuziliang@ithro.com.cn>
2019-05-08 17:18:41 +0800

    修复启动mapper注入失败的bug

--- src/main/java/com/carapp/carscrapordermanager/controller/UserContoller.java
+++ src/main/java/com/carapp/carscrapordermanager/controller/UserContoller.java
@@ -25,7 +25,6 @@ public class UserContoller {
 
     @Autowired
     private UserService userService;
-    
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
     
--- src/main/java/com/carapp/carscrapordermanager/service/imp/UserRoleServiceImpl.java
+++ src/main/java/com/carapp/carscrapordermanager/service/imp/UserRoleServiceImpl.java
@@ -2,14 +2,19 @@ package com.carapp.carscrapordermanager.service.imp;
 
 
 
+import com.carapp.carscrapordermanager.mapper.UserRoleMapper;
 import com.carapp.carscrapordermanager.pojo.UserRole;
 import com.carapp.carscrapordermanager.service.UserRoleService;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 @Service
 public class UserRoleServiceImpl extends BaseServiceImpl<UserRole> implements
 		UserRoleService {
 
+	@Resource
+	private UserRoleMapper userRoleMapper;
 
 
 }
